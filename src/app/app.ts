42942997b73a0a67199873fc60a3fe4bafb76d73
Michał Ławinski <michrarr@gmail.com>
2026-01-24 12:24:50 +0100

    Add menu component with viewchild to app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,23 @@
-import {Component, OnInit, signal, WritableSignal} from '@angular/core';
+import {Component, OnInit, signal, ViewChild, WritableSignal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Profile} from './models/profile.model';
 import {ProfileHelper} from './shared/helpers/profile.helper';
 import {NavbarComponent} from './layouts/navbar/navbar.component';
 import {MainComponent} from './pages/main.component';
+import {MenuComponent} from './layouts/menu/menu-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, MainComponent],
+  imports: [RouterOutlet, NavbarComponent, MainComponent, MenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
   protected readonly title = signal('presentation-profile');
+
+  @ViewChild('menuComponent') menuComponent!: MenuComponent;
+  @ViewChild('navbarComponent') navbarComponent!: NavbarComponent;
+  @ViewChild('mainViewComponent') mainViewComponent!: MainComponent;
 
   profile: WritableSignal<Profile> = signal<Profile>(new Profile());
 
