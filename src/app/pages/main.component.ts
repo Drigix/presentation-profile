b575dcc079ed4713630efd36cc677cad99904fef
Michał Ławinski <michrarr@gmail.com>
2026-01-24 12:25:20 +0100

    Move code from main to menu component

--- src/app/pages/main.component.ts
+++ src/app/pages/main.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit, signal} from '@angular/core';
 import {COMMON_IMPORTS, CUSTOM_COMPONENTS} from '../shared/primeng-module-import';
 import {Profile} from '../models/profile.model';
 import {Tooltip} from 'primeng/tooltip';
+import {MenuItem} from '../models/menu-item.model';
 
 @Component({
   selector: 'main-view',
@@ -10,49 +11,8 @@ import {Tooltip} from 'primeng/tooltip';
   styleUrls: ['./main.component.scss'],
   imports: [COMMON_IMPORTS, CUSTOM_COMPONENTS, Tooltip]
 })
-export class MainComponent implements OnInit{
+export class MainComponent{
 
   @Input() profile?: Profile;
-
-  menuItems = signal<any[]>([]);
-  selectedMenuItem = signal<any>(null);
-
-  ngOnInit(): void {
-    this.menuItems.set(this.loadMenuOptions());
-    this.selectedMenuItem.set(this.menuItems()[0]);
-  }
-
-  onMenuItemSelect(menuItem: any): void {
-    this.selectedMenuItem.set(menuItem);
-  }
-
-  private loadMenuOptions(): any[] {
-    return [
-      {
-        key: 'aboutme',
-        label: 'menu.aboutMe',
-        icon: 'pi pi-home'
-      },
-      {
-        key: 'education',
-        label: 'menu.education',
-        icon: 'pi pi-graduation-cap'
-      },
-      {
-        key: 'experience',
-        label: 'menu.professionalExperience',
-        icon: 'pi pi-briefcase',
-      },
-      {
-        key: 'course',
-        label: 'menu.courses',
-        icon: 'pi pi-lightbulb',
-      },
-      {
-        key: 'skills',
-        label: 'menu.skills',
-        icon: 'pi pi-sparkles'
-      }
-    ]
-  }
+  @Input() selectedMenuItem?: MenuItem;
 }
