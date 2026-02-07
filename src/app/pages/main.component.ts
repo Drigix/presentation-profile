10f0736f206c63100de6724fe5b1a754961993e0
Michał Ławinski <michrarr@gmail.com>
2026-02-07 13:36:10 +0100

    New animations in changing pages

--- src/app/pages/main.component.ts
+++ src/app/pages/main.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, OnInit, signal} from '@angular/core';
-import {COMMON_IMPORTS, CUSTOM_COMPONENTS} from '../shared/primeng-module-import';
+import {COMMON_IMPORTS, CUSTOM_COMPONENTS, PRIMENG_OVERLAY_COMPONENTS} from '../shared/primeng-module-import';
 import {Profile} from '../models/profile.model';
-import {Tooltip} from 'primeng/tooltip';
 import {MenuItem} from '../models/menu-item.model';
 
 @Component({
@@ -9,10 +8,33 @@ import {MenuItem} from '../models/menu-item.model';
   templateUrl: 'main.component.html',
   standalone: true,
   styleUrls: ['./main.component.scss'],
-  imports: [COMMON_IMPORTS, CUSTOM_COMPONENTS, Tooltip]
+  imports: [COMMON_IMPORTS, CUSTOM_COMPONENTS, PRIMENG_OVERLAY_COMPONENTS]
 })
 export class MainComponent{
 
   @Input() profile?: Profile;
-  @Input() selectedMenuItem?: MenuItem;
+
+  @Input()
+  set selectedMenuItem(selectedMenuItem: MenuItem) {
+    this.animateMenuItemClass = 'animate__animated animate__fadeOutBottomRight';
+    setTimeout(() => {
+      this.animateMenuItemClass = 'animate__animated animate__lightSpeedInLeft';
+      this._selectedMenuItem = selectedMenuItem;
+    }, 400);
+  }
+
+  get selectedMenuItem(): MenuItem | undefined {
+    return this._selectedMenuItem;
+  }
+
+  set animateMenuItemClass(animateMenuItemClass: string) {
+    this._animateMenuItemClass = animateMenuItemClass;
+  }
+
+  get animateMenuItemClass(): string | undefined {
+    return this._animateMenuItemClass;
+  }
+
+  private _selectedMenuItem?: MenuItem;
+  private _animateMenuItemClass?: string;
 }
